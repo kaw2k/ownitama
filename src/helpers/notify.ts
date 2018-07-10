71152c269fd9df8179e268955257f8d37a948290
Tucker Legard <tucker.legard@willowtreeapps.com>
2018-07-10 16:55:14 -0400

    Feature/chat notify (#2)
    * anything for rasa
    
    * add some new cards
    
    * Adds notify on chat checkbox

--- src/helpers/notify.ts
+++ src/helpers/notify.ts
@@ -1,10 +1,20 @@
 import Notify from 'notifyjs';
-
+import { ChatMessage} from '../interfaces';
 const turnNotice = new Notify('Ownitama', {body: 'It\s your turn' });
 
 export const  askPermisiion = () => { 
     if (Notify.needsPermission && Notify.isSupported() && Notify.requestPermission) {
         Notify.requestPermission();
+    }
+}
+
+export const notifyChat = (message:ChatMessage) => {
+    const chatNotice = new Notify(message.playerName, {body: message.message});
+
+    if (!Notify.needsPermission) {
+        chatNotice.show();
+    } else if (Notify.isSupported()) {
+        Notify.requestPermission(chatNotice.show);
     }
 }
 
